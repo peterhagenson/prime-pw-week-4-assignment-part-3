d79ff25758914f0369cae0a59bc00a2976eea09f
Peter Hagenson <p.hagenson@gmail.com>
2022-07-01 19:08:17 -0500

    competed required featuers

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,30 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = ["taco", "cheese", "apple"];
+
+addItem = (item) => {
+  basket.push(item);
+  return true;
+};
+
+addItem("bread");
+
+//console.log(basket);
+//console.log(addItem());
+
+listItems = () => {
+  for (let item of basket) {
+    console.log(item);
+  }
+};
+
+listItems();
+
+empty = () => {
+  basket = [];
+};
+
+empty();
+console.log(basket);
